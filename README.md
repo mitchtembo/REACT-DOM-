23edd262aeb613359bc77d98184da4dd9c871fa1
mitchtembo <mitchtembo@gmail.com>
2024-10-20 16:21:12 +0200

    Joke gen

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # React + Vite
+#In this activity I learnt about React #Hooks such as useState and useEffect, which are essential to control the state , and fetch data from APIs.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+      const data = await response.json();
+      
+       setData(data)
+    }
+    getData();
+    
+  },[])
+  return(
+    <div className="joke-container">
+      <h1 classsName="title">Joke Generator</h1>
+      {loading ? (
+        <div>
+          <h1 className="loading">Loading...</h1>
+        </div>
+      ) : (
+        data ? (
+          <div className="joke">
+            <h2 className="joke">{data.setup}</h2>
+            <h4 className="punchline">{data.punchline}</h4>
+          </div>
+        ) : (
+          <p>No joke available</p>
+        )
+      )}
+
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {console.log(data)}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      
+    </div>
   )
 }
 
